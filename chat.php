7b5689862888d5358127dff2890eac82d22cf1a4
ismailimanov <cph-ii20@cphbusiness.dk>
2017-12-17 01:21:37 +0100

    Slettet whitespaces i filer. Begyndt med chat

---
+++ chat.php
@@ -0,0 +1,11 @@
+<?php
+include("cp.header.php");
+?>
+<h1>Chat</h1>
+<span class="breadcrumbs">Kørelærer &raquo; Chat</span>
+<div class="content">
+    chat
+</div>
+<?php
+include("cp.footer.php");
+?>
